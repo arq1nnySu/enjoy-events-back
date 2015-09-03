070d2dfcc29d649c8c10fc116cc5353a76ca928e
susanrosito <rosito.susana@gmail.com>
2015-09-03 01:59:03 -0300

    Integracion con Pivotal [Finishes #102641850]

--- api.py
+++ api.py
@@ -115,10 +115,6 @@ def add_cors_headers(response):
 
     return response
 
-
-# Chequeando que todo ande bien
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-   print "Haciendo prueba para ver si funciona la integracion con pivotal :)" 
+   print "Haciendo prueba para ver si funciona la integracion con pivotal :) por segunda vez" 
