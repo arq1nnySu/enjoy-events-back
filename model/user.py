b5525c3f2a5116c882f0f964c062ab2d14cc5e73
David Leonhardt <davidleonhardt@gmail.com>
2015-10-12 00:28:20 -0300

    [#103643502] Buscando la formar de generar un hash (+salt) para el password

--- model/user.py
+++ model/user.py
@@ -1,19 +1,31 @@
 from api import db, log
 from flask.ext.mongoalchemy import BaseQuery
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class UserQuery(BaseQuery):
+    def get_by_name_and_password(self, username, password):
+        log.info("Busca un Usuario por nombre y password")
+        return self.filter(self.type.username == username and self.type.password == password)
 
-	def get_by_name_and_password(self, username, password):
-		log.info("Busca un Usuario por nombre y password")
-		return self.filter(self.type.username == username and self.type.password == password)
+    def get_by_name(self, username):
+        log.info("Busca un Usuario por nombre")
+        return self.filter(self.type.username == username)
 
-	def get_by_name(self, username):
-		log.info("Busca un Usuario por nombre")
-		return self.filter(self.type.username == username)
 
 class User(db.Document):
-	query_class = UserQuery
-	username = db.StringField()
-	password = db.StringField()
+    query_class = UserQuery
+    username = db.StringField()
+    password = db.StringField()
 
+    def __init__(self, **kwargs):
+        log.info('Creando un nuevo usuario con password = hash + salt')
+        # kwargs.update({'password': self.get_password(kwargs.pop('password'))})
+        super(db.Document, self).__init__(None, False, kwargs)
+
+    @staticmethod
+    def get_password(password):
+        return generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
--- services/jwtService.py
+++ services/jwtService.py
@@ -6,19 +6,26 @@ from model.user import User
 
 jwt = JWT(app)
 
+
 @jwt.authentication_handler
 def authenticate(username, password):
     log.info("Autenticar Usuario: {'username':'%s'}" % username)
-    return User.query.get_by_name_and_password(username, password).first()
+    db_user = User.query.get_by_name(username).first()
+    if db_user.check_password(password):
+        return db_user
+    return None
+
 
 @jwt.user_handler
 def load_user(payload):
     log.info("Cargar Usuario: {'username':'%s'}" % payload['username'])
     return User.query.get_by_name(payload['username']).first()
 
+
 @jwt.payload_handler
 def make_payload(user):
     return {'username': user.username}
+
 
 def generate_token(user):
     log.info("Generar Token para un Usuario: {'user':'%s'}" % user)
@@ -26,28 +33,34 @@ def generate_token(user):
     token = _jwt.encode_callback(payload)
     return {'token': token}
 
+
 def jwt_optional(realm=None):
     def wrapper(fn):
         @wraps(fn)
         def decorator(*args, **kwargs):
             optional_jwt(realm)
             return fn(*args, **kwargs)
+
         return decorator
+
     return wrapper
-    
+
+
 @jwt.error_handler
 def error_handler(e):
     api.abort(401, "Authorization Required")
 
+
 def currentUser():
     return _request_ctx_stack.top.current_user
 
+
 def isLogged():
-    return currentUser() != None
+    return currentUser() is not None
+
 
 def optional_jwt(realm=None):
     try:
         verify_jwt(realm)
     except JWTError:
         _request_ctx_stack.top.current_user = None
-        
